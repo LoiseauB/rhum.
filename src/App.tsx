2bd9707b89448004b91c0849b7c7fc53f24a3601
BaptL <bapt.loiseau@gmail.com>
2024-01-19 14:50:30 +0100

    style: set first sass class

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
-import './styles/App.css';
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div className="bg-secondary">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-title text-2xl text-primary">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
@@ -27,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-body text-lg text-accent">
         Click on the Vite and React logos to learn more
       </p>
     </>
