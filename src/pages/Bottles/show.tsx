6bfd0a94cea203da8c671550fb1536b4c94fc48a
Baptiste LOISEAU <baptiste@eskafactory.io>
2024-05-08 13:25:31 +0200

    feat: add comments to bottle show page

--- src/pages/Bottles/show.tsx
+++ src/pages/Bottles/show.tsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Heart } from '@phosphor-icons/react';
 
 import Note from '../../components/Note';
-import { rumBottles } from '../../config/bottles';
+import { rumBottles, rumComments } from '../../config/bottles';
 
 const BottleShow = () => {
   const { id } = useParams();
@@ -10,23 +10,44 @@ const BottleShow = () => {
   if (id && !isNaN(index)) {
     const { name, description, country } = rumBottles[index];
     const note = 4;
+    const category = 'Ambré';
     return (
       <>
         <section className="py-3 flex gap-2">
           <div className="bg-secondary size-bottle" />
           <div className="flex flex-col gap-2 size-bottle-text overflow-hidden">
-            <h1 className="text-nav text-primary">{name}</h1>
-            <p>Pays d'origine: {country}</p>
+            <div className="flex gap-4">
+              <h1 className="text-nav text-primary">{name}</h1>
+              <button>
+                <Heart size={25} className="favorite" />
+              </button>
+            </div>
+            <p>
+              Pays d'origine: <strong>{country}</strong>
+            </p>
+            <p>
+              Catégorie: <strong>{category}</strong>
+            </p>
             <p>{description}</p>
             <div className="flex w-full justify-between gap-2">
               <Note note={note} />
-              <button>
-                <Heart size={20} className="favorite" />
-              </button>
             </div>
           </div>
         </section>
         <hr className="my-4" />
+        <section>
+          <h2 className="text-lg m-b-3">Commentaires</h2>
+          <ul className="w-full flex flex-col items-center">
+            {rumComments.map(({ pseudo, comment }, index) => (
+              <li
+                key={'comm' + index}
+                className="my-2 border p-2 comments-w bg-secondary-15">
+                <p className="font-bold text-secondary m-b-1">{pseudo}</p>
+                <p>{comment}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
       </>
     );
   }
