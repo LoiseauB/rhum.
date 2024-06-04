4ea4b93cf1d7545ed29b213b59c3b722a01727d1
Baptiste LOISEAU <baptiste@eskafactory.io>
2024-06-04 17:03:41 +0200

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://rhum-back.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
