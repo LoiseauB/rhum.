054ae5c129e5b8fef3e26a07647971f31d25ac72
Baptiste LOISEAU <baptiste@eskafactory.io>
2024-06-05 11:31:38 +0200

    feat: add plugin for robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'robots.txt',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
