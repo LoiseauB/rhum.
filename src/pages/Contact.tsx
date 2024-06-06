b229c6a965467db4322b1bc7f0d140833b0484f4
Baptiste LOISEAU <baptiste@eskafactory.io>
2024-06-06 21:36:03 +0200

    feat: contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,102 @@
+import { FormEvent, useEffect, useState } from 'react';
+import Button from '../components/common/Button';
 import useTitle from '../hooks/useTitle';
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
   useTitle('Contact');
+  const [fromEmail, setFromEmail] = useState<string>();
+  const [fromName, setFromName] = useState<string>();
+  const [subject, setSubject] = useState<string>();
+  const [emailMessage, setEmailMessage] = useState<string>();
+  const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (fromEmail && fromName && subject && emailMessage) {
+      return setIsSubmit(true);
+    }
+    alert('Tous les champs doivent être renseignés');
+  };
+
+  useEffect(() => {
+    if(isSubmit) {
+      fetch(`${import.meta.env.VITE_API_HOST}/email`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          fromEmail,
+          fromName,
+          subject,
+          emailMessage,
+        })
+      }).then(response => response.json())
+        .then(data => {
+          if(data.Messages[0].Status === "success") {
+            alert('Message envoyé');
+            navigate(0);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          alert('Une erreur est survenu veuillez réessayer plus tard')
+        })
+    }
+  }, [isSubmit])
+
   return (
     <>
       <h1 className="text-xl m-b-2">Nous contacter</h1>
       <p>
-        Si vous rencontrez un problème sur notre site vous pouvez{' '}
-        <a className="text-accent" href="mailto:bapt.loiseau+rhum@gmail.com">
-          nous conctater
-        </a>
+        Si vous rencontrez un problème sur notre site vous pouvez nous conctater
       </p>
+      <section className="flex justify-center w-full m-t-5">
+        <form
+          onSubmit={e => handleSubmit(e)}
+          className="p-3 border flex flex-col gap-2 bg-secondary-50">
+          <label className="flex flex-col">
+            Email:
+            <input
+              type="email"
+              className="border p-1 text-lg bg-white"
+              required
+              onChange={e => setFromEmail(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col">
+            Nom ou pseudo:
+            <input
+              type="text"
+              className="border p-1 text-lg bg-white"
+              required
+              onChange={e => setFromName(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col">
+            Sujet:
+            <input
+              type="text"
+              className="border p-1 text-lg bg-white"
+              required
+              onChange={e => setSubject(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col">
+            Message:
+            <textarea
+              className="border p-1 bg-white"
+              required
+              onChange={e => setEmailMessage(e.target.value)}
+            />
+          </label>
+          <div className="flex flex-end">
+            <Button>Envoyer</Button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
